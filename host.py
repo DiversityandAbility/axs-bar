8dfe2c7754ea7fa2d880f1f5e2889be942b0c974
marq de Villiers <HP-LOVE-CRAFT@outlook.com>
2018-07-11 16:10:31 +0100

    minimal app with example code

--- host.py
+++ host.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+#q is the name of the page being requested
+#function returns the contence of request
+def spitPage(q):
+    with open(q, "r") as f:
+        return f.read();
+
 @app.route("/")
 def hello():
-    return "this is an example host"
+    return spitPage("wellcome.html")
+
+@app.route("/bar")
+def main():
+    return spitPage("example.html")
