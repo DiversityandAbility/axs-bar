ca6b7b9b0738e8aded74dd3b310d5bea75f3328e
marq de Villiers <HP-LOVE-CRAFT@outlook.com>
2018-07-15 22:01:13 +0100

    simple check for alredy present, working on have script self remove if posible

--- egBar.js
+++ egBar.js
@@ -1,4 +1,30 @@
+function checkForSelf() {
+  if (document.getElementById('UTilBar') == null) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function establishCan() {
+  let can = document.createElement("canvas");
+  can.id = "UTilBar";
+  can.width = window.innerWidth;
+  can.height = 32;
+
+  document.getElementsByTagName('body')[0].appendChild(can);
+  let ctx = can.getContext("2d");
+  ctx.fillStyle = "#FF00FF";
+  ctx.fillRect(0, 0, can.width, can.height);
+}
+
 function init() {
-  console.log(document.domain);
-  document.body.style.background = 'pink';
+  if (!checkForSelf()) {
+    console.log(document.domain);
+    document.body.style.background = 'pink';
+
+    establishCan();
+  } else {
+    console.log("establish failed, bar alredy present");
+  }
 }
